c5a0533550137455f6f8eb3077255f409e44fceb
anton_skomarovskyi <anton.skomarovskyi@gmail.com>
2019-09-11 11:38:39 +0300

    Add 2 tabs and initialize project

---
+++ ReutersTT/ReutersTT/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReutersTT
+//
+//  Created by Anton Skomarovskyi on 9/11/19.
+//  Copyright © 2019 Anton Skomarovskyi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReutersTT/ReutersTT/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReutersTT
+//
+//  Created by Anton Skomarovskyi on 9/11/19.
+//  Copyright © 2019 Anton Skomarovskyi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
